020bda433d4291fb30343ef56a51b211d44d3a98
narendra-sharma <devexpert2016@gmail.com>
2024-01-25 18:38:51 +0530

    Update auto-complte-feild.jsx

--- src/components/core/auto-complte-feild.jsx
+++ src/components/core/auto-complte-feild.jsx
@@ -4,11 +4,11 @@ export function CustomAutoComplete ({value,onChange}) {
     const { ref } = usePlacesWidget({
         apiKey: gapi,
         onPlaceSelected: (place) => {
-            onChange(place?.formatted_address)
+            console.log(place);
         },
         options: {
             types: ["(regions)"],
         },
     });
-    return <input ref={ref} style={{borderColor:'#EAEEF6' , fontSize:'14px'}} defaultValue={value} />;
+    return <input ref={ref} style={{borderColor:'#EAEEF6' , fontSize:'14px'}} defaultValue={value} onChange={(e) => onChange(e.target.value)}/>;
 }
