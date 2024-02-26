4fb0a9a832729a09383221edba1f75e560dd92e0
manyavv <ajayidefcoinfotec@gmail.com>
2024-02-26 10:46:20 +0530

    Multi languague structure

---
+++ src/langs/es.js
@@ -0,0 +1,10 @@
+export default {
+    "translation": {
+        "Dashboard": "panel",
+        "Devices": 'Dispositivos',
+        "Settings": "ajustes",
+        "Admin": "Administradora",
+        "Tenants": "Inquilinas",
+        "User": 'Usuaria'
+    }
+}
